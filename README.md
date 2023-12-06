78367d31b5a0340f16681152f5a87327334582ee
RHETT CHASE <rhettjchase@gmail.com>
2023-12-05 16:51:48 -0800

    complete back-end functionality

--- README.md
+++ README.md
@@ -6,6 +6,11 @@
 ## Overview
 <!-- Provide a high level overview of what this application is and why you are building it, beyond the fact that it's an assignment for this class. (i.e. What's your problem domain?) -->
 
+Query parameters:
+lat: Latitude of the city searched
+lon: Longitude of the city searched
+searchQuery: City Name searched
+
 ## Getting Started
 <!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
 
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 // Read in the shopping list from our "Database" (weather.json)
-const data = require('./data/weather.json');
+const weatherData = require('./data/weather.json');
 
 const app = express();
 
@@ -13,9 +13,43 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (request, response) => {
-  console.log('GET /');
-  response.send('movies for you');
+class Forecast {
+  constructor(date, low, high, weather) {
+    this.date = date;
+    this.low = low;
+    this.high = high;
+    this.weather = weather;
+    this.description = `Low of ${low}, high of ${high} with ${weather}`;
+  }
+}
+
+function findCityInfo(lat, lon, searchQuery) {
+  return weatherData.find((city) => {
+    const latMatch = city.lat === lat.toString();
+    const lonMatch = city.lon === lon.toString();
+    const searchQueryMatch =
+      city.city_name.toLowerCase() === searchQuery.toLowerCase();
+    if (latMatch && lonMatch && searchQueryMatch) {
+      return city;
+    }
+  });
+}
+
+app.get('/weather', (request, response) => {
+  const { lat, lon, searchQuery } = request.query;
+  const cityInfo = findCityInfo(lat, lon, searchQuery);
+  //   console.log(cityInfo);
+  const cityForecasts = cityInfo.data.map(
+    (element) =>
+      new Forecast(
+        element.datetime,
+        element.low_temp,
+        element.max_temp,
+        element.weather.description
+      )
+  );
+  console.log(cityForecasts);
+  response.json(cityForecasts);
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
